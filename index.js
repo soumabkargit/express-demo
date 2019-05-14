260eb86e890194e89251569db39a5c1bd1f59bf2
Abdoul-Karim SOUMAHORO <soumabkar@gmail.com>
2019-05-14 17:44:19 +0200

    START HTTP

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var _ = require('underscore');
 var result = _.contains([1,2,3],2);
 console.log(result);
+
+const express = require('express');
+const app = new express();
+app.get('/', (req, res)=>{
+res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res)=>{
+    res.send([1,2,3]);
+});
+ app.listen(3000, ()=> console.log('Listening on port 3000'));
+
